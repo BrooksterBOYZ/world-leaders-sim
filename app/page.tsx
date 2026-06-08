7c77ce5cc6af8d20befc70db18a5af9ace336b53
Brooks Tafreshi <brtafreshi2023@gmail.com>
2026-06-08 16:29:19 -0500

    Added homepage

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>World Leaders Simulator</h1>
+
+      <h2>Choose a Country</h2>
+
+      <button>USA</button>
+      <button>Germany</button>
+      <button>India</button>
+      <button>Russia</button>
+      <button>Brazil</button>
+    </main>
+  );
+}
